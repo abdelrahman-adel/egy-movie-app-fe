d97ce3fd48a398b23b277fd83c18f9876bf57901
Abdelrahman Adel <abdelrahman.adel@atos.net>
2023-05-15 11:20:45 +0300

    Create movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import SearchIcon from "./search.svg"
+import { MovieCard } from "./MovieCard";
+
+// API Key 9c347a1c
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    const [currPage, setCurrPage] = useState(1);
+    const [showNextPrevBtns, setShowNextPrevBtns] = useState(false);
+
+    const API_URL = "https://www.omdbapi.com/?apikey=9c347a1c"
+
+    const searchMovies = async (title, page = 1) => {
+        const response = title ? await fetch(`${API_URL}&s=${title}&page=${page}`) : await fetch(API_URL);
+        const data = await response.json();
+
+        setMovies(data.Search);
+        setShowNextPrevBtns(data?.totalResults > 10 ? true : false);
+    };
+
+    return (
+        <div className="app">
+            <h1>Egy Movie Land</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies you want"
+                    value={searchText}
+                    onChange={(event) => setSearchText(event.target.value)}
+                />
+                <img src={SearchIcon} alt="Search" onClick={() => searchMovies(searchText)} />
+            </div>
+
+            {
+                movies?.length > 0 ?
+                    <div className="container">
+                        {
+                            movies.map(movie => (
+                                <MovieCard movie={movie} />
+                            ))
+                        }
+                    </div>
+                    :
+                    <div className="empty">
+                        <h2>No movies found!!</h2>
+                    </div>
+            }
+            {
+                showNextPrevBtns ?
+                    <div className="container">
+                        <div className="search" >
+                            <button onClick={() => {
+                                setCurrPage(currPage => currPage + 1);
+                                searchMovies(searchText, currPage + 1);
+                            }}>Next Page</button>
+                        </div>
+                        <div className="empty">
+                            <h2>Page: {currPage}</h2>
+                        </div>
+                        <div className="search" >
+                            <button onClick={() => {
+                                setCurrPage(currPage => currPage - 1);
+                                searchMovies(searchText, currPage - 1);
+                            }}>Previous Page</button>
+                        </div>
+                    </div>
+                    :
+                    <></>
+            }
+        </div>
+    );
 }
 
 export default App;
